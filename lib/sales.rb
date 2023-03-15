777a9deafb5e5ea6f810e724a24d1f720d7ae09a
Luis Carlos <luis.carlos.finde@gmail.com>
2023-03-15 12:19:53 -0600

    Updating sales
    Change to module and complement flow

--- lib/sales.rb
+++ lib/sales.rb
@@ -1,2 +1,33 @@
-class Sales 
+require 'json'
+require_relative 'item'
+module Sales
+  # We are using Json as an input
+  file = File.read('./examples/example-1.json')
+
+  data = JSON.parse(file)
+
+  items = []
+  grand_total = 0
+  taxes_total = 0
+
+  puts "Input:"
+  data['items'].each do |item|
+    item = Item.new(item['quantity'], item['price'], item['category'], item['description'])
+    grand_total = grand_total + item.total
+    taxes_total = taxes_total + item.taxes
+
+    # For print purposes
+    items.push(item)
+    puts "#{item.quantity} #{item.description} at #{item.price}"
+  end
+
+  # For print purposes
+  puts ""
+  puts "Output:"
+  items.each do |item|
+    puts "#{item.quantity} #{item.description}: #{sprintf("%0.02f", item.total)}"
+  end
+
+  puts "Sales Taxes: #{sprintf("%0.02f", taxes_total)}"
+  puts "Total: #{sprintf("%0.02f", grand_total)}"
 end
--- spec/sales_spec.rb
+++ spec/sales_spec.rb
@@ -1,3 +1,11 @@
-RSPect.describe Sales do
-  
+require 'sales'
+
+RSpec.describe Sales do
+  describe "#calculate_total" do
+    let(:sales) { described_class.new }
+
+    it 'when one item is added' do
+      # expect(sales.calculate_total)
+    end
+  end
 end
